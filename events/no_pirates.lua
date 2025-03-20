8f49b18b7597fb2b3033a830360bc55687ddb94f
Edgar Simo-Serra <ess@waseda.jp>
2025-03-20 22:45:40 +0900

    Initial commit.

---
+++ events/no_pirates.lua
@@ -0,0 +1,10 @@
+--[[
+<?xml version='1.0' encoding='utf8'?>
+<event name="No Pirates">
+ <location>load</location>
+ <chance>100</chance>
+</event>
+--]]
+function create()
+   diff.apply("No Pirates")
+end
